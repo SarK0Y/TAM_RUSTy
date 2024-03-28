9c502a50b9e85dcb30f23b5ce2bf148916ff683e
SarK0Y <z0dchiy8@gmail.com>
2024-03-28 03:41:25 +0300

    /+++/

--- src/subs.rs
+++ src/subs.rs
@@ -3,15 +3,15 @@ use crate::{run_cmd_out_sync, getkey, swtch::print_pg_info};
 /*Run it Later or Cancel Now */
 pub(crate) fn rilocan(){
     let now = crate::checkArg("-now");
-    let cmd = String::from_iter(crate::get_arg_in_cmd("-rilocan").s);
+    let cmd = String::from_iter(crate::get_arg_in_cmd("-rilocan").s).trim_end_matches('\0').to_string();;
     let mut sleep = String::from_iter(crate::get_arg_in_cmd("-sleep").s).trim_end_matches('\0').trim_start().to_string();
+    let sleep0 = sleep.clone();
     let hour = 3600u64; let min = 60u64;
     let mut len_sleep = sleep.chars().count();
     let mut sleep_val = 47u64;
     if len_sleep == 0{sleep = "47s".to_string(); len_sleep = 3}
     let mut hms = String::new();
     hms.push(sleep.chars().nth(len_sleep - 1).unwrap());
-    println!("hms {}, sleep {}", hms, sleep.len());
     match hms.as_str(){
         "h" => {
             sleep = sleep.replace("h", "");
@@ -32,18 +32,27 @@ pub(crate) fn rilocan(){
             panic!("Command must look like..\ntam.rst -rilocan \"sudo standby\" -sleep 10m -now\ntam.rst -rilocan \"sudo standby\" -sleep 215s -now\ntam.rst -rilocan \"sudo standby\" -sleep 3h")
         }
     }
-        let msg = format!("You have run task {cmd} for each {sleep}\nPlease, hit any key to cancel Your task.");
+        let msg = format!("You have run task «{cmd}» for each {sleep0}\nPlease, hit any key to cancel Your task.");
         println!("{}", msg);
-        std::thread::spawn(move||{
-            loop {
-                crate::run_term_app1(cmd.clone());
-                std::thread::sleep(std::time::Duration::from_secs(sleep_val));
-                println!("Executed at {}", crate::Local::now())
-            }
-        });
+        if now {
+            std::thread::spawn(move||{
+                loop {
+                    crate::run_term_app1(cmd.clone());
+                    println!("Executed at {}", crate::Local::now());
+                    std::thread::sleep(std::time::Duration::from_secs(sleep_val));
+                }
+            });
+    } else {
+               std::thread::spawn(move||{
+                loop {
+                    std::thread::sleep(std::time::Duration::from_secs(sleep_val));
+                    crate::run_term_app1(cmd.clone());
+                    println!("Executed at {}", crate::Local::now())
+                }
+            });
+    }
         crate::getkey();
-        println!("Have a nice day, DEAR USER|n
-        See You Soon 🙃", );
+        println!("Have a nice day, DEAR USER.\nSee You Soon 🙃", );
         std::process::exit(0);
 
     }
