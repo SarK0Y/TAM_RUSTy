66755c2a486166da001ce54cd99f1614ea5885a9
SarK0Y <sark0y@protonmail.com>
2024-05-01 05:37:18 +0300

    some gears to patch lists :)
    very simple trick for integration w/ gdb :)

--- src/in_dbg0.rs
+++ src/in_dbg0.rs
@@ -1,9 +1,14 @@
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
-use once_cell::sync::Lazy;
+use once_cell::sync::Lazy; use substring::Substring;
 
 pub(crate) fn breaks(name: &str, id: u64, justRet: bool) -> (u64, bool){
     static mut br: Lazy<HashMap<String, u64>> = Lazy::new(||{HashMap::new()});
+    if name.substring(0, 2) == "d:"{
+        let name = name.replace("d:", "").trim_end().trim_start().to_string();
+       crate::C!(br.remove(&name)); return (0, false);
+    }
+
     match crate::C!(br.entry(name.to_string())){
         Entry::Occupied(entry) => {return (*entry.get(), true)},
         Entry::Vacant(entry) => {if !justRet {entry.insert(id); println!("inserted {}", unsafe{br.get(&name.to_string()).unwrap_or(&u64::MAX)}); crate::getkey();} return (id, false)}
@@ -16,5 +21,8 @@ pub(crate) fn manage_breaks(cmd: &String){
     println!("id: {id}");
     let id = crate::globs18::strn_2_u64(id).unwrap();
     breaks(&name, id, false);
+}
+pub(crate) fn just_break(){
+    println!("Just break")
 }
 // fn
