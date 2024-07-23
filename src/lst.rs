c0bd9270ab8ec800fe741b5476e3a0c9ff3665ce
SarK0Y <sark0y@protonmail.com>
2024-07-23 12:49:18 +0300

    v 1.9.181
    fixes lst command.

--- src/lst.rs
+++ src/lst.rs
@@ -357,8 +357,10 @@ pub(crate) fn manage_lst(cmd: &String){
         let head = read_tail(&item, "/");
         let item = full_escape(&item);
         let link_2_item = full_escape(&format!("{}/{}", take_list_adr("env/lst"), head) );
-        let cmd = format!("ln -sf {item} {link_2_item}");
-        run_cmd0(cmd);
+        if link_2_item != item{
+            let cmd = format!("ln -sf {item} {link_2_item}");
+            run_cmd0(cmd);
+        }
         mark_front_lst(&head); set_front_list2(&head, 0); crate::fix_num_files(711284191);return;
     }
     }
