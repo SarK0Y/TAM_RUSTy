4ff989a4158ffee5534cd6400daa96d6a5d728e6
SarK0Y <sark0y@protonmail.com>
2024-08-09 18:20:29 +0300

    v 2.2.12
    some tunes for lst

--- src/lst.rs
+++ src/lst.rs
@@ -11,7 +11,7 @@ use crate::custom_traits::{STRN, STRN_strip, fs_tools};
 #[cfg(feature = "mae")]
 use Mademoiselle_Entropia::help_funcs::get_file;
 use crate::update18::delay_ms;
-use crate::{helpful_math_ops, mk_empty_file, run_cmd_out_sync, save_file, save_file0, save_file_append, save_file_append_newline, set_prnt, tailOFF, turn_2_i64};
+use crate::{fix_num_files, helpful_math_ops, mk_empty_file, run_cmd_out_sync, save_file, save_file0, save_file_append, save_file_append_newline, set_prnt, tailOFF, turn_2_i64};
 use crate::{globs18::{take_list_adr, split_once_alt, check_char_in_strn, take_list_adr_env, strn_2_usize, get_item_from_front_list}, errMsg0, read_file, patch_t, split_once, read_tail, parse_paths, run_term_app, is_dir2, escape_backslash, escape_apostrophe, escape_symbs, getkey, dont_scrn_fix, popup_msg, full_escape, mk_dummy_file, ending, run_cmd0, mark_front_lst, set_front_list2, usize_2_i64, get_path_from_strn, name_of_front_list, no_esc_t};
 
 use std::io::BufRead;
@@ -342,6 +342,7 @@ pub(crate) fn list_the_lists(){
         run_cmd0(cmd);
     }
     mark_front_lst("lst"); set_front_list2("lst", 0);
+    fix_num_files(-79814395);
 }
 pub(crate) fn manage_lst(cmd: &String){
     let cmd0 =cmd.to_string();
