0f0a5bf36a453c8ebbc7a738ff53d28383a1cc60
SarK0Y <sark0y@protonmail.com>
2024-09-20 17:00:36 +0300

    Neovim extras.

--- src/nvim.ext.rs
+++ src/nvim.ext.rs
@@ -1,6 +1,6 @@
 use std::fmt::format;
 use once_cell::sync::Lazy;
-use crate::{mkdir, split_once, STRN};
+use crate::{mkdir, read_midway_data, split_once, STRN};
 
 pub fn add_keys_2_cmd (cmd: &String) -> String {
     match cmd.as_str() {
@@ -27,17 +27,29 @@ pub fn nvim_remote (cmd: &String) {
 pub fn nvimc (nvim: Option < String >) -> String {
     "".strn()
 }
-pub fn nvim_version () -> Option < (String, String ) > {
-   let mut nvv = r"nvim --version|grep -i 'nvim\sv[0-9]*\.[0-9]*\.[0-9]*'".strn();
+pub fn nvim_version (app_name: &str) -> Option < (String, String ) > {
+    let mut nvv = format! (r"{app_name} --version|grep -i 'nvim\sv[0-9]*\.[0-9]*\.[0-9]*'" );
    let ret = crate::run_cmd_out_sync( nvv );
     if ret != "" { nvv = ret }
     else { crate::errMsg0( "Err: Unknown Neovim is installed on Your system."); return None}
    let (_, nvv ) = crate::split_once(&nvv, " ") ;
    let nvv = nvv.replace ("v", "");
-   let (high, nvv) = split_once(&nvv, ".");
-   let (middle, nvv) = split_once(&nvv, ".");
+   let ( high, nvv) = split_once(&nvv, ".");
+   let ( middle, nvv) = split_once(&nvv, ".");
    let low = nvv;
-
+   let base_ver = (0u16, 10u32, 1u16);
+   let high: Option< u16 > = match high.parse:: <u16> () {Ok(x) => Some(x), _ => None };
+   if high == None { crate::errMsg0( "Err: Unknown Neovim is installed on Your system."); return None}
+   if high.unwrap() < base_ver.0 { crate::errMsg0( &format!("Required minimal version of Neovim: {:#?}", base_ver) ); return None}
+   if high.unwrap() > base_ver.0 { return Some( (app_name.strn(), format! ("{}.{}.{}", high.unwrap(), middle, low ) ) )}
+   let middle: Option< u32 > = match middle.parse:: <u32> () {Ok(x) => Some(x), _ => None };
+   if middle == None { crate::errMsg0( "Err: Unknown Neovim is installed on Your system."); return None}
+   if middle.unwrap() < base_ver.1 { crate::errMsg0( &format!("Required minimal version of Neovim: {:#?}", base_ver) ); return None}
+   if middle.unwrap() > base_ver.1 { return Some( (app_name.strn(), format! ("{}.{}.{}", high.unwrap(), middle.unwrap(), low ) ) )}
+   let low: Option< u16 > = match low.parse:: <u16> () {Ok(x) => Some(x), _ => None };
+   if low == None { crate::errMsg0( "Err: Unknown Neovim is installed on Your system."); return None}
+   if low.unwrap() < base_ver.2 { crate::errMsg0( &format!("Required minimal version of Neovim: {:#?}", base_ver) ); return None}
+   if low.unwrap() > base_ver.2 { return Some( (app_name.strn(), format! ("{}.{}.{}", high.unwrap(), middle, low.unwrap() ) ) )}
 
     None
 }
