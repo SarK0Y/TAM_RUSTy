e75d1ad0c447baf8f0a91b02fb49bfe3b9e7e0ef
SarK0Y <sark0y@protonmail.com>
2024-10-05 01:46:19 +0300

    better dbg :)

--- src/threadpool.rs
+++ src/threadpool.rs
@@ -1,9 +1,11 @@
 use once_cell::sync::Lazy;
 use nix::sys::wait::wait;
 use nix::unistd::{fork, ForkResult, getpid, getppid, execve};
+use std::env;
 use std::{ ffi::CString, env::var, num::NonZero };
+use crate::globs18::take_list_adr;
 use crate::update18::delay_secs;
-use crate::{errMsg0, helpful_math_ops, split_once, STRN};
+use crate::{errMsg0, getkey, helpful_math_ops, save_file_append_newline_abs_adr_fast, split_once, STRN};
 pub fn thr_ids ( mode: crate::enums::threadpool ) {
     static mut ids: Lazy < Vec < nix::unistd::Pid > > = Lazy::new ( || { Vec::with_capacity (100) } );
     unsafe {
@@ -15,7 +17,7 @@ pub fn thr_ids ( mode: crate::enums::threadpool ) {
 }
 pub fn new_thr (cmd: &String) {
    match unsafe { fork() } {
-        Ok(ForkResult::Parent { child }) => { thr_ids (crate::enums::threadpool::add_new( child ) ); return; },
+        Ok(ForkResult::Parent { child }) => { thr_ids (crate::enums::threadpool::add_new( child ) ); wait().expect ("failed to wait"); },
         Ok(ForkResult::Child) => { run_kid(cmd ); std::process::abort();},
         Err(err) => { eprintln!("Fork failed: {}", err); },
     }    
@@ -38,23 +40,39 @@ pub fn run_kid (cmd: &String) {
         loop {
             let (arg, cmd0 ) = split_once(&cmd, " ");
             cmd = cmd0;
-           // dbg!(&arg); delay_secs(3);
+         //   dbg!(&arg); delay_secs(3);
             if arg == "none" { break }
             args[ cnt ] = c_str (&arg); cnt.inc();
         }
-        //dbg!(&args); dbg!(&app_name); delay_secs(31);
-        execve ( &c_str ( &app_name), &args, form_env (&mut env) );
+        form_env (&mut env);
+       /* let mut env_prnt = |env: & [CString]| {
+            for i in 0..6 {
+                dbg! (env [i] );
+            }
+        }; */
+       //dbg!(&args); dbg!(&app_name); dbg! ( &env); delay_secs(12);
+        use nix::errno::Errno;
+        match execve ( &c_str ( &app_name), &args, &env ) {
+            Err(e) =>  {logErr(e );},
+            _ =>              {}
+        };
         errMsg0 ("execve failed");
     }
 }
 pub fn form_env <'a > (env_str: &'a mut [CString] ) -> &'a [CString] {
 //    let mut env_vec: Vec < String > = Vec::new();
     let mut count = 0usize;
-    let key = format! ("PATH={}\0",  var ("PATH").unwrap_or( "".strn() ) );
+    let key = format! ("PATH={}",  var ("PATH").unwrap_or( "".strn() ) );
     env_str[ count ] =  CString::new (key.as_str() ).unwrap_or( CString::new("").unwrap() );
     count.inc();
         env_str
 }
+pub fn logErr (e: nix::errno::Errno ) {
+    let log_err_file = take_list_adr ("ErrNumLog");
+    let err = format!("{e:#?}");
+    save_file_append_newline_abs_adr_fast( &err, &log_err_file);
+}
+//fn
 /*
 use nix::unistd::{execve, fork, ForkResult};
 
