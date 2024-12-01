a80fe9596cd3a74712f8c98f0ee0e58fabf6cee8
SarK0Y <sark0y@protonmail.com>
2024-12-01 21:09:47 +0300

    v 13.1.0
    algo1 (async) to morph wav files. :)

--- src/nui.morph.rs
+++ src/nui.morph.rs
@@ -98,12 +98,14 @@ pub fn read_chan_f32 (
     ch_num: usize, 
     num_of_channels: usize, start_from: usize, range: usize ) -> Vec < f32> {
     let mut ret: Vec < f32 > = Vec::new ();
-    let mut to = (range + start_from );
-    if to > (samples.len() + start_from ) { to = samples.len() + start_from ; }
-    dbg! (&to);
-    for i in start_from..to {
-        let cursor = i * num_of_channels + ch_num;
-        if cursor > to { break;}
+    if start_from >= samples.len() { return ret;};
+    let mut to = range;
+    if to + start_from > samples.len() { to = samples.len() - start_from; }
+    let upto = to + start_from;
+    dbg! (&to); dbg! (&start_from);
+    for i in 0..to {
+        let cursor = start_from + i * num_of_channels + ch_num;
+        if cursor >= upto  { break;}
         ret.push ( samples [ cursor ] );
     }
     ret
@@ -112,13 +114,14 @@ pub fn write_chan_f32 (
     samples: &mut [f32], 
     ch_num: usize, 
     num_of_channels: usize, start_from: usize, patch: &Vec < f32 > ) -> usize {
-    let mut to = (patch.len() + start_from );
-    if to > samples.len() { to = samples.len(); }
+    let mut to = patch.len();
+    if to > samples.len() { to = samples.len() - start_from; }
+    let upto = to + start_from;
     let mut cursor = 0usize;
     let mut prev = cursor;
-    for i in start_from..to {
-        cursor = i * num_of_channels + ch_num;
-        if cursor > to { break;}
+    for i in 0..to {
+        cursor = start_from + i * num_of_channels + ch_num;
+        if cursor > upto { break;}
         samples [ cursor ] = patch [ i ];
         prev = cursor - ch_num;
     } dbg! (&cursor); prev
