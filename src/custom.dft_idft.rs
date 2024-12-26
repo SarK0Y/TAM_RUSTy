8849a0f19ac660cd57a77a79855933ff7bd55b18
SarK0Y <sark0y@protonmail.com>
2024-12-26 07:10:38 +0300

    v 13.9.4
    corrections, optimizations & improvements.

--- src/custom.dft_idft.rs
+++ src/custom.dft_idft.rs
@@ -13,16 +13,19 @@ pub fn custom_dft (samples: &mut [f32],
         freq: Vec::<f32>::new(),
         phase: Vec::<f32>::new(),
     };
-    let mut z_sample = Complex::new (0.0, 0.0);
+    let mut z_sample: Complex<f32> = Complex::new (0.0, 0.0);
     let norm_to = to - from;
-    let j = Complex::new (0.0, 1.0);
+    let j: Complex<f32> = Complex::new (0.0, 1.0);
     for freq0 in 0..1_000_000_000 {
-        let freq = (spectre.from + spectre.step * freq0 as f32) * norm_to as f32;
+        let freq = (spectre.from + spectre.step * freq0 as f32);
         if freq > spectre.to {break;}
         for t in 0..norm_to {
-            let coef = alt_const.powc (-2.0 * PI * freq * t as f32 * j/ norm_to as f32 );
+            let coef = alt_const.powc ((-2.0 * PI * t as f32 * j/ norm_to as f32) * freq );
             z_sample += samples[from + t] * coef;
         }
+        cdft.amplitude.push ((z_sample.re.powi(2) + z_sample.im.powi (2) ).sqrt() );
+        cdft.phase.push ((z_sample.im / z_sample.re).atan() );
+        cdft.freq.push(freq);
     }
 cdft
 }
