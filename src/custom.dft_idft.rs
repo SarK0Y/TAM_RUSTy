9ccecba4fc450ce4eea5de0872aa48890d78c406
SarK0Y <sark0y@protonmail.com>
2025-01-17 00:53:53 +0300

    v 13.10.44
    corrections, optimizations & improvements.

--- src/custom.dft_idft.rs
+++ src/custom.dft_idft.rs
@@ -12,14 +12,10 @@ use crate::enums::freq_range;
 use crate::errMsg0;
 use crate::ret0;
 pub fn get_freq_component_of_sample (sample: f32, time: usize, freq: f32) -> f32 {
-    /*
-    let sample = phi_sine(cdft.freq[s], t, cdft.phase[s]) * cdft.amplitude[s];
-                let sample_phi = mock_sine(cdft.freq[s], t ) * cdft.amplitude[s];
-                samples[t] += sample; 
-                let z_sample = samples [t] * E.powc (-2.0 * Complex::<f32>::i() * PI * cdft.freq[s] * t as f32);
-                let amplitude = (z_sample.re.powi (2) + z_sample.im.powi (2) ).sqrt() ;
-     */
-    0.0
+    let z_sample = sample * E.powc (-2.0 * Complex::<f32>::i() * PI * freq * time as f32);
+    let amplitude = (z_sample.re.powi (2) + z_sample.im.powi (2) ).sqrt() ;
+    let phase = (z_sample.re / amplitude).acos();
+    phi_sine(freq, time, phase ) * amplitude
 }
 pub fn custom_dft (samples: &mut [f32], 
     from: usize,
