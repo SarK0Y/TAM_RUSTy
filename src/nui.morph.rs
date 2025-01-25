792095e144087a53974092fcebc0ea8f2e103c65
SarK0Y <sark0y@protonmail.com>
2025-01-25 22:53:18 +0300

    v 13.10.69
    corrections, optimizations & improvements.

--- src/nui.morph.rs
+++ src/nui.morph.rs
@@ -63,6 +63,7 @@ pub fn universum_vox_morph0 (duration: u16, path_to_conf: &String) {
         19 => {mk_morph_alg19_exclude_freqs( &mut samples, &uv_morph ); },
         20 => {mk_morph_alg20_exclude_freqs( &mut samples, &uv_morph ); },
         21 => {mk_morph_alg21_replace_freqs( &mut samples, &uv_morph ); },
+        21 => {mk_morph_alg22_wave_energy( &mut samples, &uv_morph ); },
         _ => {mk_morph_alg0( &mut samples, &uv_morph ); },
     }
     //let file_name = format! ( "Universum Vox.{}.wav", mk_uid( 24 ));
@@ -267,19 +268,21 @@ pub fn mk_morph_alg22_wave_energy(samples: &mut [f32], uv: &crate::enums::univer
     use crate::faav::over_samples;
     use crate::faav::over_samples0;
     let over_uv_: *const crate::enums::universum_vox_morph = uv;
-    crate::faav::over_uv( Some (over_uv_ ) );
+    //crate::faav::over_uv( Some (over_uv_ ) );
      let mut ch0: Vec <_> = read_chan_f32(samples, 0, 2, 0, samples.len() );
      let mut over_ch0: *mut [f32] = &mut *ch0;
      over_samples(Some (over_ch0) );
     let mut thr1 = std::thread::spawn (move|| 
-        {mk_morph_alg21_replace_freqs_ (
-           unsafe { &mut *over_samples(None).unwrap() }, unsafe { &*over_uv(None).unwrap() } ); });
+        {crate::cdsp::tune_wave_energy (
+           unsafe { &mut *over_samples(None).unwrap() },
+           unsafe { (*over_uv(None).unwrap()).scale.unwrap_or(0.23) } ); });
      let mut ch1: Vec <_> = read_chan_f32(samples, 1, 2, 0, samples.len() );
      let mut over_ch1: *mut [f32] = &mut *ch1;
      over_samples0(Some (over_ch1) );
      let mut thr2 = std::thread::spawn (move|| {
-        mk_morph_alg21_replace_freqs_ (
-            unsafe { &mut *over_samples0(None).unwrap() }, unsafe { &*over_uv(None).unwrap() } ); });
+        crate::cdsp::tune_wave_energy (
+           unsafe { &mut *over_samples(None).unwrap() },
+           unsafe { (*over_uv(None).unwrap()).scale.unwrap_or(0.23) } ); });
     thr1.join();
     thr2.join();
     write_chan_f32(samples, 0, 2, 0, &ch0 );
