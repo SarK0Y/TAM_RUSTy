726ca08e0faca4abd3059288b2e3c81d14b4a47e
SarK0Y <sark0y@protonmail.com>
2025-01-29 02:50:44 +0300

    v 13.11.3
    corrections, optimizations & improvements.

--- src/nui.morph.rs
+++ src/nui.morph.rs
@@ -66,6 +66,7 @@ pub fn universum_vox_morph0 (duration: u16, path_to_conf: &String) {
         22 => {mk_morph_alg22_wave_energy( &mut samples, &uv_morph ); },
         23 => {mk_morph_alg23_wave_energy( &mut samples, &uv_morph ); },
         24 => {mk_morph_alg24_rev( &mut samples, &uv_morph ); },
+        25 => {mk_morph_alg25_wave_energy( &mut samples, &uv_morph ); },
         _ => {mk_morph_alg0( &mut samples, &uv_morph ); },
     }
     //let file_name = format! ( "Universum Vox.{}.wav", mk_uid( 24 ));
