ec5d1ac357dd01f58004548d6f853f6811b14f16
SarK0Y <sark0y@protonmail.com>
2025-03-30 19:20:01 +0300

    v 13.11.155
    cdsp

--- src/nui.morph.rs
+++ src/nui.morph.rs
@@ -320,6 +320,32 @@ pub fn mk_morph_alg26_wave_energy(samples: &mut [f32], uv: &crate::enums::univer
     write_chan_f32(samples, 0, 2, 0, &ch0 );
     write_chan_f32(samples, 1, 2, 0, &ch1 );
 }
+pub fn mk_morph_alg31_shuffle(samples: &mut [f32], uv: &crate::enums::universum_vox_morph ) {
+    use crate::faav::over_uv;
+    use crate::faav::over_samples;
+    use crate::faav::over_samples0;
+    let over_uv_: *const crate::enums::universum_vox_morph = uv;
+    crate::faav::over_uv( Some (over_uv_ ) );
+     let mut ch0: Vec <_> = read_chan_f32(samples, 0, 2, 0, samples.len() );
+     let mut over_ch0: *mut [f32] = &mut *ch0;
+     over_samples(Some (over_ch0) );
+    let mut thr1 = std::thread::spawn (move|| 
+        {crate::cdsp::tune_wave_energy_mix1 (
+           unsafe { &mut *over_samples(None).unwrap() },
+           unsafe { &*over_uv(None).unwrap() } ); });
+     let mut ch1: Vec <_> = read_chan_f32(samples, 1, 2, 0, samples.len() );
+     let mut over_ch1: *mut [f32] = &mut *ch1;
+     over_samples0(Some (over_ch1) );
+     let mut thr2 = std::thread::spawn (move|| {
+        crate::cdsp::tune_wave_energy_mix1 (
+           unsafe { &mut *over_samples0(None).unwrap() },
+           unsafe { &*over_uv(None).unwrap() } ); });
+    thr1.join();
+    thr2.join();
+    write_chan_f32(samples, 0, 2, 0, &ch0 );
+    write_chan_f32(samples, 1, 2, 0, &ch1 );
+}
+
 pub fn mk_morph_alg29_wave_energy(samples: &mut [f32], uv: &crate::enums::universum_vox_morph ) {
     use crate::faav::over_uv;
     use crate::faav::over_samples;
