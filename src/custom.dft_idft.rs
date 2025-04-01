4efb89b612ed2eca594765686e6cd6dc21474975
SarK0Y <sark0y@protonmail.com>
2025-04-01 16:29:09 +0300

    v 13.11.169
    cdsp

--- src/custom.dft_idft.rs
+++ src/custom.dft_idft.rs
@@ -518,8 +518,8 @@ pub fn tune_wave_energy_stat (samples: &mut [f32], uv: &crate::enums::universum_
     let window_width = input [0];
     let sum = input_f32 [0];
     let mut if_keys: Vec <u8> = Vec::with_capacity (window_width as usize);
-    if_keys [0] = 0;
-    for t in 1..if_keys.len() {
+    if_keys.push (0);
+    for t in 1..window_width as usize {
         if_keys [t] = 1;
     }
     let mut fns: Vec <fn (String)> = Vec::new();
