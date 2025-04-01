10ca74230990c3a7d60c0b45295d4f732d53ae4d
SarK0Y <sark0y@protonmail.com>
2025-04-01 10:07:33 +0300

    v 13.11.161
    cdsp

--- src/custom.dft_idft.rs
+++ src/custom.dft_idft.rs
@@ -511,8 +511,9 @@ pub fn shuffle (mut samples: &mut [f32], uv: &crate::enums::universum_vox_morph)
     }
 }
 pub fn tune_wave_energy_stat (samples: &mut [f32], uv: &crate::enums::universum_vox_morph) {
+    if uv.input_u64.is_none() {errMsg0("'input_64' in Universum Vox must be set"); return}
     for j in 0..samples.len() {
-        
+
     }
 }
 pub fn tune_wave_energy3 (samples: &mut [f32], uv: &crate::enums::universum_vox_morph) {
