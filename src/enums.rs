ed41d44421d25d1a2547810d4a12d2f8feee95e2
SarK0Y <sark0y@protonmail.com>
2025-04-01 10:00:48 +0300

    v 13.11.160
    corrections, optimizations & improvements.

--- src/enums.rs
+++ src/enums.rs
@@ -172,6 +172,7 @@ pub struct universum_vox_morph {
     pub range: Option <f32 >,
     pub scale: Option <f32 >,
     pub coef: Option <Vec <f32> >,
+    pub input_u64: Option <Vec <u64> >,
     pub plus_minus_freq: Option < bool >,
     pub geoms: Option <Vec <geom> >,
     pub select_channel: Option <u8>, 
