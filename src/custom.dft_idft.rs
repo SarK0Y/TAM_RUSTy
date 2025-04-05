21207eb0c45330ddd57b549efc0888b571ec6a24
SarK0Y <sark0y@protonmail.com>
2025-04-05 19:39:11 +0300

    v 13.11.193
    corrections, optimizations & improvements.

--- src/custom.dft_idft.rs
+++ src/custom.dft_idft.rs
@@ -453,6 +453,18 @@ pub fn tune_wave_energy2_ (samples: &mut [f32], uv: &crate::enums::universum_vox
         base = base1;
     }
 }
+pub fn smooth_wave_energy (samples: &mut [f32], uv: &crate::enums::universum_vox_morph) {
+    let mut base = samples [0];
+    let mut base1 = samples [ 1 ];
+    let energy_dt = uv.step_freq.unwrap_or (0.23);
+    let scale = uv.scale.unwrap_or (0.31) * -1.0;
+    for j in 1..samples.len() {
+        base1 = samples [ j ];
+        samples [j] = base - base1 + energy_dt;
+        samples [ j ] *= scale;
+        base = base1;
+    }
+}
 pub fn tune_wave_energy2 (samples: &mut [f32], uv: &crate::enums::universum_vox_morph) {
     let mut base = samples [0];
     let mut base1 = samples [ 1 ];
