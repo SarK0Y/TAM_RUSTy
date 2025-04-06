b8b718dc0adb8145f4b8085ae846963fcb251736
SarK0Y <sark0y@protonmail.com>
2025-04-06 14:53:14 +0300

    v 13.11.196
    cdsp

--- src/custom.dft_idft.rs
+++ src/custom.dft_idft.rs
@@ -471,6 +471,24 @@ pub fn smooth_wave_energy (samples: &mut [f32], uv: &crate::enums::universum_vox
         }
     }
 }
+pub fn smooth_wave_energy1_ (samples: &mut [f32], uv: &crate::enums::universum_vox_morph) {
+    if uv.input_u64.is_none() {errMsg0("'input_u64' in Universum Vox must be set"); return}
+    //if uv.input_f32.is_none() {errMsg0("'input_f32' in Universum Vox must be set"); return}
+    let input_u64 = uv.input_u64.clone().unwrap();
+    if input_u64.len() < 3 {errMsg0("'input_u64' in Universum Vox sets 'step_width', 'from' & 'to'"); return}
+   // let input_f32 = uv.input_f32.clone().unwrap();
+    let step_width = input_u64 [0] as usize;
+    let from = input_u64 [1] as usize + 1;
+    let to = if input_u64 [2] == 0 { samples.len() } else {input_u64 [2] as usize };
+    let mut j = from + step_width;
+    let mut tmp = 0_f32;
+    while j < to {
+        tmp = (samples [j - step_width ] + samples [ j ] ) / 2.0;
+        for i in j - step_width + 1..j {
+            samples [ i ] = tmp;
+        }
+    }
+}
 pub fn tune_wave_energy2 (samples: &mut [f32], uv: &crate::enums::universum_vox_morph) {
     let mut base = samples [0];
     let mut base1 = samples [ 1 ];
