5b1d350ee5cb12eb1a12c0afec3aa482f7582c8e
SarK0Y <sark0y@protonmail.com>
2025-04-16 04:04:35 +0300

    v 13.11.232
    cmath

--- src/custom_math.rs
+++ src/custom_math.rs
@@ -3,7 +3,7 @@ pub fn poly <T: std::ops::Mul<Output = T> +
                 std::ops::Add<Output = T> +
                 std::ops::AddAssign +
                 std::ops::MulAssign +
-                std::ops::BitXor<Output = T> +
+//                std::ops::BitXor<Output = T> +
                 PartialEq +
                 Eq +
                 std::ops::Div<Output = T> +
@@ -11,12 +11,12 @@ pub fn poly <T: std::ops::Mul<Output = T> +
                 Copy +
                 std::fmt::Debug +
                 PowIt <T> >
-        (coefs: Vec <T>, x: T, ret: &mut T) {
-            if coefs.len () == 0 {return}
+        (coefs: Vec <T>, x: T ) -> T {
+            if coefs.len () == 0 {return x}
             let mut sum: T =  x - x;
             for i in 0..coefs.len() {
-                sum += x.pow ( i );
-            }
+                sum += x.pow ( i ) * coefs [ i ];
+            } return sum
         
 }
 pub trait PowIt <T: std::ops::Mul<Output = T> +
@@ -27,7 +27,7 @@ pub trait PowIt <T: std::ops::Mul<Output = T> +
                 PartialEq +
                 Eq +
                 std::ops::Div<Output = T> +
-                std::ops::BitXor<Output = T> +
+  //              std::ops::BitXor<Output = T> +
                 std::cmp::PartialOrd +
                 Copy +
                 std::fmt::Debug >{
@@ -41,23 +41,24 @@ impl <T: std::ops::Mul<Output = T> +
                 PartialEq +
                 Eq +
                 std::ops::Div<Output = T> +
-                std::ops::BitXor<Output = T> +
+    //            std::ops::BitXor<Output = T> +
                 std::cmp::PartialOrd +
                 Copy +
                 std::fmt::Debug > PowIt <T> for T {
     fn pow (&self, exp: usize) -> T {
+        if exp == 0 {return *self / *self;}
         dbg!(&exp);
-        let mut norm_exp = exp as u64;
-        let mut ret: T = *self ^ *self;
+        let mut norm_exp = exp;
+        let mut ret: T = *self / *self;
         let mut sq = *self;
         while norm_exp > 0 {
-            dbg!(&norm_exp);
+ //           dbg!(&norm_exp);
             if norm_exp & 1 == 1 {
                 ret *= sq;
-                dbg! (&ret);
+              //  dbg! (&ret);
             } sq *= sq;
-            dbg! (&sq);
+           // dbg! (&sq);
             norm_exp /= 2;
-        } ret
+        } return ret
     }
 }
