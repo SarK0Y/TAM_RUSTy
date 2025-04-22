043eb1c590776f6bec8653e7d7fb7a2663e2a3f1
SarK0Y <sark0y@protonmail.com>
2025-04-22 13:11:13 +0300

    v 13.11.261
    npf

--- src/extra/npf.rs
+++ src/extra/npf.rs
@@ -3,6 +3,7 @@ use std::ops::MulAssign;
 use rug::float::Round;
 use rug::ops::{AddAssignRound, DivAssignRound, MulAssignRound, PowAssign as rugPowAssign, PowAssignRound, SubAssignRound, Pow as rugpow};
 use rug::{Assign, Integer as rugint, float::Constant as rugconst, Float as rugfloat, ops::SubFrom};
+use crate::errMsg0;
 #[derive(Debug, Clone, PartialEq)]
 pub struct init_form {
     pub head: rugfloat,
@@ -73,6 +74,7 @@ pub fn tst (a: &init_form, b: &init_form) -> product_form {
     return a * b;
 }
 pub fn npf (n: rugfloat) -> (rugfloat, rugfloat) {
+    let mut ret = (rugfloat::with_val (0), rugfloat::with_val (0));
     let mut X = init_form::mk (4, 3);
     let mut Y = init_form::mk (4, 3);
     let mut X_tst: Vec < init_form > = Vec::new();
@@ -81,17 +83,38 @@ pub fn npf (n: rugfloat) -> (rugfloat, rugfloat) {
     let mut y = rugfloat::with_val (3);
     let init = init_form::new ();
     let init0 = init_form::mk (2, 0);
+    let mut count_positive_results = 0_usize;
+    let mut j = 0_usize;
+    let mut mark_j = j;
     let mut finally = Vec:: <product_form>::new ();
     while X.num1 () * Y.num1 () < n {
         X_tst.push ( X.nest ( init0 ) );
         X_tst.push ( X.nest ( init ) );
-        Y_tst.push ( X.nest ( init0 ) );
-        Y_tst.push ( X.nest ( init ) );
-        
+        Y_tst.push ( Y.nest ( init0 ) );
+        Y_tst.push ( Y.nest ( init ) );
+        finally.push (X_tst [0] * Y_tst [0]); // even-even
+        finally.push (X_tst [1] * Y_tst [1]); // odd-odd
+        finally.push (X_tst [1] * Y_tst [0]); // odd-even
+        finally.push (X_tst [0] * Y_tst [1]); // even-odd
+        for fin in finally {
+            if (n - fin.tail) % X_tst [0].head == 0 {count_positive_results += 1; mark_j = j;};
+            j += 1;
+        }
+        if count_positive_results > 1 {errMsg0("Simple NPF failed."); return ret};
+        match mark_j {
+            0 => {X = X_tst [0]; Y = Y_tst [0]},
+            1 => {X = X_tst [1]; Y = Y_tst [1]},
+            2 => {X = X_tst [1]; Y = Y_tst [0]},
+            3 => {X = X_tst [0]; Y = Y_tst [1]},
+        }
+        j = 0;
+        count_positive_results = 0;
         X_tst.clear();
         Y_tst.clear ();
+        finally.clear ();
     }
-    return (rugfloat::with_val (0), rugfloat::with_val (0))
+    if X.num1 () * Y.num1 () == n {ret.0 = X.num1 (); ret.1 = Y.num1()}
+    return ret
 }
 //fn
 /*
