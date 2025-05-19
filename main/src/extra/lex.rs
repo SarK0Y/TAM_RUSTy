654aede43d827152d1e594582bdf52c644339746
SarK0Y <sark0y@protonmail.com>
2025-05-19 17:00:40 +0300

    v 13.11.384
    goto

--- main/src/extra/lex.rs
+++ main/src/extra/lex.rs
@@ -1,4 +1,15 @@
 use crate::custom_traits::STRN;
-pub fn read_token (key: &String) -> Option < String > {
+pub fn read_token (key: &String, txt: &String) -> Option < String > {
+    if txt.len() == 0 { return None}
+    let mut ret = key.clone ();
+    let key_len = key.chars().count();
+    let mut pos: usize = if let Some (x) = txt.find (key) { x + key_len } else {return None };
+    let mut chars = txt.chars();
+    let mut ch = chars.nth(0).unwrap();
+    let stop_ch = ":".strn().chars().nth(0).unwrap();
+    for j in pos..txt.chars().count() {
+        ch = chars.nth(j).unwrap();
+        if ch != stop_ch { ret.push(ch);}
+    }
     None
 }
