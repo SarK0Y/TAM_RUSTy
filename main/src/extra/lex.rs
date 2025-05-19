be7eb50fb536527de48e513c194aebab74f51e9c
SarK0Y <sark0y@protonmail.com>
2025-05-19 17:24:44 +0300

    v 13.11.385
    goto

--- main/src/extra/lex.rs
+++ main/src/extra/lex.rs
@@ -1,15 +1,21 @@
 use crate::custom_traits::STRN;
-pub fn read_token (key: &String, txt: &String) -> Option < String > {
+pub fn read_token (key: &String, txt: &String) -> Option < Vec <String> > {
     if txt.len() == 0 { return None}
-    let mut ret = key.clone ();
+    let mut ret0 = key.clone ();
     let key_len = key.chars().count();
-    let mut pos: usize = if let Some (x) = txt.find (key) { x + key_len } else {return None };
+    //let mut pos: usize = if let Some (x) = txt.find (key) { x + key_len } else {return None };
+    let mut pos: Vec <usize> = txt.match_indices (key).map (|(j, _)| j + key_len ).collect();
     let mut chars = txt.chars();
     let mut ch = chars.nth(0).unwrap();
     let stop_ch = ":".strn().chars().nth(0).unwrap();
-    for j in pos..txt.chars().count() {
-        ch = chars.nth(j).unwrap();
-        if ch != stop_ch { ret.push(ch);}
+    let mut out = Vec:: <String>:: new ();
+    while let Some (nxt_pos) = pos.pop() {
+        let mut ret = ret0.clone();
+        for j in nxt_pos..txt.chars().count() {
+            ch = chars.nth(j).unwrap();
+            if ch != stop_ch { ret.push(ch);}
+        }
+        out.push(ret);
     }
-    None
+    return Some (out.clone() )
 }
