62a8e95d4856afbcb8b98ba17e6793592fccf6f6
SarK0Y <sark0y@protonmail.com>
2025-05-29 02:23:36 +0300

    v 13.11.401
    some tsts

--- ext/goto/src/lib.rs
+++ ext/goto/src/lib.rs
@@ -6,6 +6,13 @@ use syn::{parse_macro_input, parse_quote, ItemFn, LitStr, Stmt, Meta, MetaList, 
 token::Comma, Expr, Lit};
 use proc_macro2::{TokenStream as TokenStream2, Span};
 mod lex;
+   macro_rules! _prnt_func {
+    ($name:ident, $body:block) => {
+        pub fn $name() {
+            println!("{}", stringify!($body));
+        }
+    };
+}
 #[proc_macro_attribute]
 pub fn inject(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let mut input: ItemFn = parse_macro_input!(item as ItemFn);
@@ -52,11 +59,16 @@ pub fn inject_after_hello(_attr: TokenStream, item: TokenStream) -> TokenStream 
 
     TokenStream::from(quote! { #input })
 }
+//#![feature(trace_macros)]
+//trace_macros!(true);
 #[proc_macro_attribute]
 pub fn inject_tst(args: TokenStream, item: TokenStream) -> TokenStream {
+   let sub_fn = prnt_func ("lets_prnt_func", item.clone());
+   return sub_fn.clone();
     let item_str = format! ("{}", item.clone() );
     let mut ext_quote = quote!();
-    let mut input: ItemFn = parse_macro_input!(item as ItemFn);
+    let item0 = item.clone();
+    let mut input: ItemFn = parse_macro_input!(item0 as ItemFn);
     let lit: LitStr = LitStr::new("Alice", proc_macro2::Span::call_site());
     let val = Expr::Lit(syn::ExprLit {
         attrs: vec![],
@@ -92,10 +104,9 @@ pub fn inject_tst(args: TokenStream, item: TokenStream) -> TokenStream {
         }
         Meta::NameValue(name_value) => { 
             let ident = name_value.path.get_ident().unwrap();
-            let ident = format! ("{}", ident);
             let value = name_value.value;
         ext_quote.extend (
-           quote! { println!("Name-value attribute: {} = {}", 
+           quote! { println!("Name-value attribute: {} = {:?}", 
                    #ident,
                    #value);
             } );
@@ -149,25 +160,19 @@ pub fn inject_tst(args: TokenStream, item: TokenStream) -> TokenStream {
     let add_to: Stmt = syn::parse_quote! {
                 println!("yst {}\n{}\n{}", #strn_stmts, #input_str, #item_str);
             };
-    let ext_quote_stmt: Stmt = parse_quote! { #ext_quote };
     let stmts = &input.block.stmts;
     let stmts_len = stmts.len();
-    for j in 2..stmts_len {
-        if j == stmts_len -1 {new_stmts.push ( add_to.clone() ); new_stmts.push ( ext_quote_stmt.clone() ); }
+    for j in 0..stmts_len {
+        if j == stmts_len -1 {new_stmts.push ( add_to.clone() ); }
         new_stmts.push(stmts [j].clone());
     }            
-    input.block.stmts = new_stmts.clone(); 
-    let sub_prnt: TokenStream = prnt_func ("sub_prnt", item.clone() );
+    input.block.stmts = new_stmts; 
     let input_str = format! ("{:#?}", input);
     let ext_quote_str = ext_quote.to_string();
-     return TokenStream::from(quote! { 
-        #sub_prnt
+    return TokenStream::from(quote! { 
         pub fn lets_prnt_func () {
-            sub_prnt();
-            //println! ("input {} ext {}", #input_str, #ext_quote_str ); 
-            #(#new_stmts)*
+            println! ("input {} ext {}", #input_str, #ext_quote_str );
             }} )
-   // return TokenStream::from(quote! { #input } )
 }
 #[proc_macro]
 pub fn my_macro(input: TokenStream) -> TokenStream {
@@ -201,15 +206,30 @@ pub fn my_macro(input: TokenStream) -> TokenStream {
             pub fn lets_prnt_func () {
                 println! ("{}", #func_body);
             }
-        }; return out.into()
+        };
+        return out.into()
    }
-   pub fn prnt_func (name_fn: &str, input: TokenStream) -> TokenStream {
+   #[inline]
+   fn prnt_func (name_fn: &str, input: TokenStream) -> TokenStream {
         let func_body = input.to_string();
+        let name_fn = strn_to_Ident (&name_fn);
+        // let fn_name = parse_macro_input!( name_fn as LitStr ).value();
+    
+    // Convert the string to an Ident
+    //let name_fn = syn::Ident::new(&name_fn, proc_macro2::Span::call_site());
         let out = quote! {
             pub fn #name_fn () {
                 println! ("{}", #func_body);
             }
-        }; return out.into()
+        };
+       // let out: ItemFn = parse_quote!( out as ItemFn );
+        return out.into()
    }
+fn strn_to_LitStr(s: &str) -> LitStr {
+    LitStr::new(s, Span::call_site())
+}
+fn strn_to_Ident(s: &str) -> syn::Ident {
+    syn::Ident::new(s, Span::call_site())
+}
 //pub use crate::inject_after_hello; 
 // https://www.freecodecamp.org/news/procedural-macros-in-rust/
--- main/src/main.rs
+++ main/src/main.rs
@@ -417,7 +417,7 @@ use goto1717::just_prnt;
 #[cfg(feature ="tst_macro")]
 //#[inject_tst(tt0 = "fooo", tst0="nxt ffoo")]
 #[inject_tst(tst1="tst")]
-#[just_prnt]
+//#[just_prnt]
 fn tst () { println!("tst here"); println!("end here" );}
 fn self_dive(nm: String){// just sidekick to crrash tst :)
     std::thread::spawn(||{
