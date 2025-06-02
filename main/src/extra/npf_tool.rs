09c7a95ece3d2bcb2eb8fda3603a83ca9085f876
SarK0Y <sark0y@protonmail.com>
2025-06-02 22:39:17 +0300

    v 13.11.421
    npf tool

--- main/src/extra/npf_tool.rs
+++ main/src/extra/npf_tool.rs
@@ -24,37 +24,31 @@ pub fn B_base_num_sys (num: &rugint, rdx: &rugint) -> Vec < rugint > {
         num /= rdx.clone();
     } return conv
 }
-pub fn continued_fraction_approximation(x: f64, max_terms: usize, len_in_bits: u32, ) -> (rugfloat, rugfloat) {
-    let mut float_x = rugfloat::with_val(len_in_bits, x);
+pub fn continued_fraction_approximation(x: rugfloat, max_terms: usize, len_in_bits: u32, ) -> (rugfloat, rugfloat) {
+    let mut float_x = x.clone();
     let mut a = float_x.clone().trunc(); 
     let mut numerator = a.clone(); 
     let mut denominator = rugfloat::with_val(len_in_bits, 1.0); 
     let mut prev_numerator = rugfloat::with_val(len_in_bits, 1.0); 
     let mut prev_denominator = rugfloat::with_val(len_in_bits, 0.0);
 
-    float_x -= &a; // Update to the fractional part
+    float_x -= &a;
 
     for _ in 0..max_terms {
         if float_x.is_zero() {
-            break; // Stop if there's no fractional part
+            break; 
         }
 
         // Take the reciprocal
         float_x = rugfloat::with_val(len_in_bits, 1) / float_x;
         a = float_x.clone().trunc();
-
-        // Update the convergents
         let new_numerator = (&a * &numerator + &prev_numerator).complete( len_in_bits.into () );
         let new_denominator = (&a * &denominator + &prev_denominator).complete(len_in_bits.into() );
-
-        // Update for the next iteration
         prev_numerator = numerator;
         prev_denominator = denominator;
         numerator = new_numerator;
         denominator = new_denominator;
-
-        // Update the fractional part
-        float_x -= &a; // Update to the new fractional part
+        float_x -= &a; 
     }
 
     return (numerator, denominator)
