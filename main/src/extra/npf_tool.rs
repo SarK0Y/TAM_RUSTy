8edacfa123ca4416218557316b36abdd86547f7c
SarK0Y <sark0y@protonmail.com>
2025-06-04 03:52:05 +0300

    v 13.11.426
    npf_tool

--- main/src/extra/npf_tool.rs
+++ main/src/extra/npf_tool.rs
@@ -54,7 +54,7 @@ pub fn continued_fraction_approximation(x: rugfloat, max_terms: usize, len_in_bi
     return (numerator, denominator)
 }
 pub fn z_approx (n: &rugint, approx_accuracy: u32) -> rugfloat {
-    let Z = rugfloat::with_val(approx_accuracy, &n) + rugfloat::with_val (approx_accuracy, 9.0);
+    let Z = rugfloat::with_val(approx_accuracy, n) + rugfloat::with_val (approx_accuracy, 9.0);
     todo! ();
 }
 pub fn bts_npf (n: rugint, Z: &rugfloat, approx_accuracy: u32) -> (rugfloat, rugfloat) {
@@ -63,12 +63,12 @@ pub fn bts_npf (n: rugint, Z: &rugfloat, approx_accuracy: u32) -> (rugfloat, rug
     let eps = rugfloat::with_val(approx_accuracy, 2.0);
     let eps = eps.pow (approx_accuracy);
     let eps = rugfloat::with_val(approx_accuracy, 1.0) / eps;
-    let mut x: rugfloat = (&Z / &_2).complete (approx_accuracy.into() );
+    let mut x: rugfloat = (Z / &_2).complete (approx_accuracy.into() );
     let mut dx: rugfloat = x.clone() / 2;
     let mut maybe_n = rugfloat::with_val(approx_accuracy, &n);
     let mut dn = rugfloat::with_val(approx_accuracy, &n);
     loop {
-        maybe_n = x.clone() * (&Z - &x ).complete (approx_accuracy.into() );
+        maybe_n = x.clone() * (Z - &x ).complete (approx_accuracy.into() );
         if maybe_n > N {x -= &dx;} else { x += &dx; }
         dx = (&dx / &_2).complete (approx_accuracy.into() );
         dn = (maybe_n.clone() - N.clone()).abs();
