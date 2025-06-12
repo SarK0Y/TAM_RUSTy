8e26016be5ebef3bfbb616009594e9554b4812e7
SarK0Y <sark0y@protonmail.com>
2025-06-12 20:47:23 +0300

    v 13.11.457
    let's write own little lexer for Rust :)

--- ext/goto/src/edit_funx.rs
+++ ext/goto/src/edit_funx.rs
@@ -10,11 +10,13 @@ pub fn rewrite_last_exit (stream: &String, new_end: &String ) -> String {
     if last_exit.chars().count() < 3 {
         let top = stream.as_str().substring(0, stream.chars().count() - 1);
         let edit = format! ("{top}\n{new_end}\n}}");
+        leave_file_mark ("/tmp/edit", &edit);
         return edit
     }
     let edit = format! ("\n{new_end}\n{last_exit}");
     let stream0 = stream.replace (&last_exit, &edit);
     leave_file_mark ("/tmp/func0", &stream0);
+    leave_file_mark ("/tmp/last", &last_exit);
     leave_file_mark ("/tmp/edit", &edit);
     return stream0
 }
@@ -25,10 +27,11 @@ pub fn find_last_exit (stream: &String ) -> String {
     let mut count_ending: usize = 0;
     let mut chars = stream.chars();
     let mut ending = String::new();
-    let mut cursor = stream_len - 1;
+    let mut cursor = stream_len - 2;
     let mut ch: char = ' ';
     while cursor > 0 {
-        ch = chars.nth (cursor ).unwrap_or (' ');
+        ch = chars.clone().nth (cursor ).unwrap_or (' ');
+        //leave_file_mark ("/tmp/ch", &ch.to_string () );
         if ch != stop || blocks_state ( Some ( &ch ) )  { ending.push (ch ); }
         if ch == stop && !blocks_state ( Some ( &ch ) ) { break;}
         cursor.dec();
--- main/src/main.rs
+++ main/src/main.rs
@@ -418,10 +418,11 @@ use goto1717::just_prnt;
 //#[inject_tst(tt0 = "fooo", tst0="nxt ffoo")]
 //#[inject_tst(tst1="tst", tst1="jj")]
 #[prnt_vars]
-fn tst () { 
+fn tst () -> String { 
     let tst = 411u32;
     let (x, y) = (47u64, 357u32);
-println!("tst here"); println!("end here" );
+    println!("tst here"); println!("end here" );
+    return "tst".strn()
 }
 fn self_dive(nm: String){// just sidekick to crrash tst :)
     std::thread::spawn(||{
