fa37d990865a6cec3385bd120bde7bcb71e81f6c
SarK0Y <sark0y@protonmail.com>
2025-09-08 20:12:25 +0300

    min_err_per_step

--- ext/min_err_per_step/src/base.rs
+++ ext/min_err_per_step/src/base.rs
@@ -4,6 +4,7 @@ use rug::{Assign, Integer as rugint, float::Constant as rugconst, Float as rugfl
 use rug::float::Constant;
 use Mademoiselle_Entropia::minio::InterruptMsg;
 pub fn fast_n_simple_long_Pi (err: usize ) -> rugfloat {
+    let PREC0: u64 = glob_precision (None); 
     let _2 = rugfloat::with_val_64 (PREC0, 2);
     let _1 = rugfloat::with_val_64 (PREC0, 1);
     let err = _1.clone () / _2.clone().pow(err);
@@ -27,3 +28,12 @@ pub fn fast_n_simple_long_Pi (err: usize ) -> rugfloat {
         num_of_pts *= 2;
     } num_of_pts * dx
 }
+pub fn glob_precision (prec: Option <u64> ) -> u64{
+    static mut sav: u64 = 512;
+    unsafe {
+        if let Some (x) = prec {
+            sav = x;
+        } return sav
+    }
+}
+//fn
