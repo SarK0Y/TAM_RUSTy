c2547a278bae6f226beb9e6a4f3feb7d9734160c
SarK0Y <sark0y@protonmail.com>
2025-09-10 15:39:55 +0300

    min_err_per_step

--- ext/min_err_per_step/src/trig.rs
+++ ext/min_err_per_step/src/trig.rs
@@ -4,7 +4,7 @@ use rug::{Assign, Integer as rugint, float::Constant as rugconst, Float as rugfl
 use rug::float::Constant;
 use crate::base::{glob_precision, Pi };
 use Mademoiselle_Entropia::minio::InterruptMsg;
-pub fn fast_n_simple_sin3 (x: &rugfloat, err: usize ) -> rugfloat {
+pub fn fast_n_simple_sin3 (x: &rugfloat, err: u64 ) -> rugfloat {
     let PREC0 = glob_precision (None);
     let _3 = rugfloat::with_val_64 (PREC0, 3);
     let _1 = rugfloat::with_val_64 (PREC0, 1);
@@ -22,3 +22,11 @@ pub fn fast_n_simple_sin3 (x: &rugfloat, err: usize ) -> rugfloat {
     dbg! (&sin_3x);
     return sin_3x
 } 
+pub trait Trig {
+    fn __sin (&self) -> Self;
+}
+impl Trig for rugfloat {
+    fn __sin (&self) -> Self {
+        return fast_n_simple_sin3 (self, glob_precision (None) )
+    }
+}
